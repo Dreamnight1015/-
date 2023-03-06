5683dbe311ad88a7fb356a723eea2eb5ff255ecc
Dreamnight1015 <114711729+Dreamnight1015@users.noreply.github.com>
2023-03-06 10:54:55 +0900

    chungcheonguniv

--- 상담관리시스템.js
+++ 상담관리시스템.js
@@ -8,6 +8,38 @@ $(document).ready(function(){
         };
     });
     $('.tap2').click(function(){
-        
+        $('.tap1 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap2 .fa-solid').css('color','rgba(13,76,62)');
+        $('.tap3 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap4 .fa-regular').css('color','rgba(0,0,0,0.5)');
+        $('.tap5 .fa-solid').css('color','rgba(0,0,0,0.5)');
+    })
+    $('.tap1').click(function(){
+        $('.tap1 .fa-solid').css('color','rgba(13,76,62)');
+        $('.tap2 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap3 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap4 .fa-regular').css('color','rgba(0,0,0,0.5)');
+        $('.tap5 .fa-solid').css('color','rgba(0,0,0,0.5)');
+    })
+    $('.tap3').click(function(){
+        $('.tap1 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap2 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap3 .fa-solid').css('color','rgba(13,76,62)');
+        $('.tap4 .fa-regular').css('color','rgba(0,0,0,0.5)');
+        $('.tap5 .fa-solid').css('color','rgba(0,0,0,0.5)');
+    })
+    $('.tap4').click(function(){
+        $('.tap1 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap2 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap3 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap4 .fa-regular').css('color','rgba(13,76,62)');
+        $('.tap5 .fa-solid').css('color','rgba(0,0,0,0.5)');
+    })
+    $('.tap5').click(function(){
+        $('.tap1 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap2 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap3 .fa-solid').css('color','rgba(0,0,0,0.5)');
+        $('.tap4 .fa-regular').css('color','rgba(0,0,0,0.5)');
+        $('.tap5 .fa-solid').css('color','rgba(13,76,62)');
     })
 })
